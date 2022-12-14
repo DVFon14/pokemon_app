9f14695c17d3b9c8974a733fba20da0f629fe233
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-12-13 18:53:31 -0700

    working on routes

--- server.js
+++ server.js
@@ -1,26 +1,60 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const pokemon = require("./models/pokemon");
-const port = 3000;
+const port = 8000;
 
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine()); //similar to requiring
 
-// const index = require("./views/Index");
+//Connect to Mongoose and remove deprication warnings
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
 
+//INDEX <-----
 app.get("/pokemon", (req, res) => {
   res.render("Index", { pokemon: pokemon }); //first argument if file. Second argument is data. Could be pokemon: pokemon of pokemon since the key-value pair are the same
 });
 
+//POST <-----
+app.post("/pokemon", (req, res) => {
+  if (req.body.readyToPlay === "on") {
+    req.body.readyToPlay = true;
+  } else {
+    req.body.readyToPlay = false;
+  }
+  // Fruit.create(req.body, (error, createdFruit) => {
+  //   res.redirect("/pokemon");
+  // });
+});
+
+//DELETE
+
+//UPDATE
+
+//CREATE <-----
+
+//EDIT
+
+//SHOW <-----
 app.get("/pokemon/:id", (req, res) => {
   res.render("Show", {
     pokemon: pokemon[req.params.id], //pokemon: pokemon. The second is getting the index of the pokemon array
   });
 });
+
+//POST
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- views/Index.jsx
+++ views/Index.jsx
@@ -6,31 +6,9 @@ const React = require("react");
 
 const myStyle = {
     color: '#ffffff',
-    backgroundColor: '#000000',
+   // backgroundColor: '#000000',
   };
 
-  
-// export default class Index extends React.Component() {
-//   render() {
-    
-//     const pokemon = this.props.pokemon
-//     return (
-//       <div>
-//         <h1 style={myStyle}>All Pokemon!</h1>;
-//         {pokemon.map((pokemon, id)=>{ //map is an array method. This pokemon is the individul pokemon, id because we're using the array index as the id
-//             return(
-//             <div>
-//                 <a href={`/pokemon/${id}`}>
-//                     <li>{pokemon.name}</li>                    
-//                 </a>
-//             </div>
-//             );
-//         })}
-//         <a href = '/:id'></a>
-//       </div>
-//     )
-//   }
-// }
 
 const Index = ({pokemon}) => {
     return (
